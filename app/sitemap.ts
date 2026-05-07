55df4de3573534714f6472d42736cd82b74b8357
Navi Mehrok <navinderjeet@gmail.com>
2026-05-07 13:20:31 -0700

    fix(deploy): add force-static to robots and sitemap for GitHub Pages export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next';
 import { getAllPostSlugs } from '../lib/Posts';
 import { SITE_URL } from '../lib/site-url';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = SITE_URL;
 
